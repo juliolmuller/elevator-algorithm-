7825317e8f4e79a1a5134e7911670c7455987f42
Julio L. Muller <julio_muller@crgl-thirdparty.com>
2023-01-04 15:14:16 -0300

    style: define some global styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from '~/App'
-import '~/index.scss'
+import '~/styles.scss'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
