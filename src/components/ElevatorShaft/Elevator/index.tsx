1c5ca6236c6e1376e8a084c543e5a6aec9e7194f
Julio L. Muller <julio_muller@crgl-thirdparty.com>
2023-01-06 22:49:35 -0300

    feat: add animation to elevator doors

--- src/components/ElevatorShaft/Elevator/index.tsx
+++ src/components/ElevatorShaft/Elevator/index.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useRef, useState } from 'react'
+import clsx from 'clsx'
+import { TransitionEvent, useEffect, useRef, useState } from 'react'
 
 import styles from './styles.module.scss'
 
@@ -8,25 +9,38 @@ interface ElevatorProps {
 
 function Elevator({ floor }: ElevatorProps) {
   const prevFloor = useRef(0)
+  const isMounted = useRef(false)
+  const [isDoorOpen, setDoorOpen] = useState(true)
 
   function synchronizeFloorState() {
     prevFloor.current = Math.max(0, floor)
   }
 
+  function openDoor(event: TransitionEvent<HTMLDivElement>) {
+    ;(event.target as HTMLDivElement).classList.contains(styles.root) && setDoorOpen(true)
+  }
+
+  function closeDoor() {
+    isMounted.current && setDoorOpen(false)
+  }
+
   useEffect(() => {
     synchronizeFloorState()
-  })
+    isMounted.current && closeDoor()
+    isMounted.current = true
+  }, [floor]) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <div
       className={styles.root}
       style={{
+        transitionDuration: `${Math.abs(floor - prevFloor.current) * 2}s`,
         transform: `translateY(calc((100% - 10px) * -${floor}))`,
       }}
-      onTransitionEnd={(...args) => console.log(...args)}
+      onTransitionEnd={openDoor}
     >
-      <div className={styles.door} />
-      <div className={styles.door} />
+      <div className={clsx(styles.door, isDoorOpen && styles.doorOpen)} />
+      <div className={clsx(styles.door, isDoorOpen && styles.doorOpen)} />
     </div>
   )
 }
